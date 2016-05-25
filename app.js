cf46e8177298863e0da65e5d8d0cc1a24c970c2a
Karl Sjöstrand <karl.sjostrand@gmail.com>
2016-05-25 09:15:43 +0200

    Rudimentary login (google) with various redirects

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var hogan = require('hogan-express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var Sequelize = require('sequelize');
 
 var routes = require('./routes/index');
 
 var app = express();
 
+// db setup
+var sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+
+    storage: 'db.sqlite'
+});
+//var sequelize = new Sequelize('sqlite://db.sqlite');
+
+var User = sequelize.define('user', {
+    id: { type: Sequelize.STRING, primaryKey: true },
+    name: Sequelize.STRING
+});
+
+User.sync();
+
 // view engine setup
-app.set('view engine', 'hjs'); // use .html extension for templates
+app.set('view engine', 'hjs'); // use .hjs extension for templates
 app.enable('view cache');
-app.engine('html', require('hogan-express'));
+app.engine('html', hogan);
 
 
 // uncomment after placing your favicon in /public
@@ -22,8 +49,49 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+        clientID: "99967977588-c2967bf2lcupmhmdeqrak0pnul133tp9.apps.googleusercontent.com",
+        clientSecret: "GHyzJ_n23S5tumztUlh5YPaw",
+        callbackURL: "http://localhost:3000/auth/google/callback"
+    }, function(accessToken, refreshToken, profile, done) {
+        User.findOrCreate({ where: { id: profile.id }, defaults: { name: profile.displayName } })
+            .spread(function (user) {
+                return done(null, user);
+            }).catch(function (err) {
+                return done(err, null);
+            });
+    }
+));
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+    User.findById(id)
+        .then(function (user) {
+            cb(null, user);
+        }).catch(function (err) {
+            cb(err, null);
+        });
+});
 
 app.use('/', routes);
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect(req.session.returnTo || '/');
+        delete req.session.returnTo;
+    });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,6 +123,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,43 @@ var request = require('request');
 
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    request({
-        url: 'http://localhost:5000/api/metadata/patients?count=-1',
-        auth: { user: 'admin', password: 'admin' },
-        json: true
-    }, function(error, response, data) {
+router.get('/login', function(req, res) {
+    res.render('login');
+});
+
+router.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/login');
+});
+
+router.get('/', loggedIn, function(req, res, next) {
+    listPatients(function(error, response, data) {
         if (!error && response.statusCode === 200) {
-            res.render('index', {patients: data});
+            res.render('index', {patients: data, photos: req.user.photos});
         } else {
             res.send("Error: " + error + ", response: " + JSON.stringify(response));
         }
     });
 });
 
+router.get('/test', loggedIn, function (req, res, next) {
+    res.send('Test page');
+});
+
+function loggedIn(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        req.session.returnTo = req.path;
+        res.redirect('/login');
+    }
+}
+
+function listPatients(handler) {
+    return request({
+        url: 'http://localhost:5000/api/metadata/patients?count=-1',
+        auth: { user: 'admin', password: 'admin' },
+        json: true
+    }, handler);
+}
 module.exports = router;
